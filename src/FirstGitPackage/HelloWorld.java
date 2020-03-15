d823fbffecb04a4fdcb6f54f02d195dd223009b7
Nattaya Ari <arinattaya9@gmail.com>
2020-03-15 17:56:57 -0400

    first commit

---
+++ src/FirstGitPackage/HelloWorld.java
@@ -0,0 +1,10 @@
+package FirstGitPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello guys!");
+	}
+
+}
